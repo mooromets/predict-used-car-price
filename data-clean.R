7af5245ef459f92c5916ec13f3284eeed2ae9bd5
mooromets <sergey.sambor@gmail.com>
2017-10-05 19:20:06 +0200

    updated data-clean, added State variable

--- data-clean.R
+++ data-clean.R
@@ -4,26 +4,44 @@
 library(tidyverse)
 library(forcats)
 auto <- read_csv("./data/autos.csv")
-clean_auto<-filter(auto, brand!="sonstige_autos", # brand clearly identified
-                   offerType!="Gesuch", # only offer
-                   seller=="privat", #Sell by private individuals (not dealers)
-                   price< 150000, price>100,# Exclude prices lower than 100Euros or higher than 150000 Euros
-                   yearOfRegistration>1975, yearOfRegistration<2017, #Exclude cars older than 1975 or after 2016
-                   powerPS<700 # Exclude cars were power higher than 700 HP
-)
-clean_auto<-mutate (clean_auto, 
-                    fuelType = ifelse(is.na(fuelType), "andere", fuelType), # Reasign cases where fuelType= NA to "andere" (other)
-                    vehicleType = ifelse(is.na(vehicleType), "andere", vehicleType) # Reasign cases where vehicleType are NA's to "andere" (other)
-)
-clean_auto<-select (clean_auto, -c(dateCrawled, seller, offerType, abtest, monthOfRegistration,
-                                   nrOfPictures, dateCreated, lastSeen))
-## drop all those variables
+
+clean_auto<-filter(auto, brand != "sonstige_autos", # brand clearly identified
+                   offerType != "Gesuch", # only offer
+                   seller == "privat", #Sell by private individuals (not dealers)
+                   price < 150000, price > 100, # Exclude prices lower than 100Euros or higher than 150000 Euros
+                   yearOfRegistration > 1975, yearOfRegistration < 2017, #Exclude cars older than 1975 or after 2016
+                   powerPS < 700, # Exclude cars were power higher than 700 HP
+                   powerPS > 0 # exclude zeros 
+                  ) %>%
+            # drop some variables
+            select (-c(dateCrawled, seller, offerType, abtest, monthOfRegistration,
+                       nrOfPictures, dateCreated, lastSeen)
+                    ) %>%
+            # adjust extraordinary low kilometer values to more trusty ones
+            # (insurance companies in Germany use 15000 km/year in their formulas), 
+            # but we allow 10000 km/year as a more or less sensible minimun 
+            mutate (kilometer = ifelse((2017 - yearOfRegistration) * 10000 <= kilometer, 
+                                            kilometer, 
+                                           (2017 - yearOfRegistration) * 10000
+                                           )
+                    )
+
+postal <- read.csv("./data/de_postal_codes.csv", 
+                   colClasses = c("character", NULL, NULL, "factor", rep(NULL, 4)))
+
+clean_auto <- left_join(clean_auto,
+                        postal[ ,c("Postal.Code", "State.Abbreviation")],
+                        by = c("postalCode" = "Postal.Code"))
 
 ## convert to factor
-
 clean_auto$brand <- factor(clean_auto$brand)
 clean_auto$gearbox <- factor(clean_auto$gearbox)
 clean_auto$postalCode <- factor(clean_auto$postalCode)
 clean_auto$fuelType <- factor(clean_auto$fuelType)
 clean_auto$notRepairedDamage <- factor(clean_auto$notRepairedDamage)
 clean_auto$vehicleType <- factor(clean_auto$vehicleType)
+clean_auto$State.Abbreviation <- factor(clean_auto$State.Abbreviation)
+
+
+## free memory
+rm(auto, postal)
