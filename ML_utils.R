f8abc9b657449f343347b3f5e381553003d6ccea
mooromets <sergey.sambor@gmail.com>
2018-03-20 14:11:11 +0100

    updated brand subsetting with a multiple-choice availability

--- ML_utils.R
+++ ML_utils.R
@@ -1,6 +1,9 @@
 
-getBrandDataset <- function(data, brand) {
-  out <- data[data[, "brand"] == brand, ]
+# get a subset from data with only selected brands
+# remove incomplete cases
+# remove columns not used in training
+prepareBrandDataset <- function(data, brand) {
+  out <- data[data[, "brand"] %in% brand, ]
   # remove NAs
   out <- out[complete.cases(out), ]
   # convert y to factor
@@ -8,10 +11,10 @@ getBrandDataset <- function(data, brand) {
   # remove useless columns:
   out[, -which(colnames(out) %in% 
                       c("Longitude", "Latitude", "postalCode", 
-                        "State.Abbreviation", "brand", "name"))]
+                        "State.Abbreviation", "name"))]
 }
 
-
+#fit a model with the specified seed before training
 defModelFit <- function(method, seed, ...) {
   print(system.time({
     set.seed(seed)
