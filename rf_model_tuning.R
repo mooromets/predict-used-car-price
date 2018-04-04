08ca0a6fecb758c794add24f2f7d65871470dd75
mooromets <sergey.sambor@gmail.com>
2018-04-04 14:36:07 +0200

    tuneRF tool

--- rf_model_tuning.R
+++ rf_model_tuning.R
@@ -13,6 +13,7 @@ dataset <- remove0variance(fixMissingFactors(vw[sample(nrow(vw),
 seed <- 8877
 metric <- "Accuracy"
 
+
 # Random search
 control <- trainControl(method="cv", number=4, search="random")
 set.seed(seed)
@@ -21,6 +22,7 @@ rf_random <- train(model~., data=dataset, method="rf", metric=metric, tuneLength
 print(rf_random)
 plot(rf_random)
 
+
 #grid search
 control <- trainControl(method="cv", number=4, search="grid")
 set.seed(seed)
@@ -28,6 +30,14 @@ tunegrid <- expand.grid(.mtry=c(1:15))
 rf_gridsearch <- train(model~., data=dataset, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
 print(rf_gridsearch)
 plot(rf_gridsearch)
+
+
+# Algorithm Tune (tuneRF)
+set.seed(seed)
+bestmtry <- tuneRF(dataset[, -6], dataset[, 6], stepFactor=2, improve=1e-5, ntree=500)
+print(bestmtry)
+plot(bestmtry[,1], bestmtry[,2], type = "l")
+
 
 # Number of trees 
 control <- trainControl(method="cv", number=4)
