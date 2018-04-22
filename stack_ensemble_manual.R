af3fbf206355893a649ec7810e8a12708aac4003
mooromets <sergey.sambor@gmail.com>
2018-04-22 21:32:30 +0200

    fixed errors in model ensembling

--- stack_ensemble_manual.R
+++ stack_ensemble_manual.R
@@ -14,34 +14,35 @@ predictors <- colnames(trainSet)[!colnames(trainSet) %in% outcome]
 preProc <- c("center", "scale")
 
 control <- trainControl(method="cv", 
-                        number=4) #,savePredictions = 'final', classProbs = T)
+                        number=6 ,savePredictions = 'final', classProbs = T)
 
-model_rf <- train(trainSet[, predictors], trainSet[, outcome], 
-                  method = "rf", trControl = control)
-model_knn <- train(trainSet[, predictors], trainSet[, outcome], 
-                   method = "knn", trControl = control, preProcess = preProc)
-model_c50 <- train(trainSet[, predictors], trainSet[, outcome], 
-                   method = "C5.0", trControl = control)
+model_rf <- train(x = trainSet[, predictors], 
+                  y = trainSet[, outcome], 
+                  method = "rf", 
+                  trControl = control)
+model_knn <- train(trainSet[, predictors], trainSet[, outcome], method = "knn", trControl = control, preProcess = preProc)
+model_c50 <- train(trainSet[, predictors], trainSet[, outcome], method = "C5.0", trControl = control)
+
 
 trainSet$OOF_pred_rf <- model_rf$pred$pred[order(model_rf$pred$rowIndex)]
 trainSet$OOF_pred_knn <- model_knn$pred$pred[order(model_knn$pred$rowIndex)]
 trainSet$OOF_pred_c50 <- model_c50$pred$pred[order(model_c50$pred$rowIndex)]
 
-testSet$OOF_pred_rf <- predict(model_rf, testSet["model"], type='prob')$pred
-testSet$OOF_pred_knn <- predict(model_knn, testSet["model"], type='prob')$pred
-testSet$OOF_pred_c50 <- predict(model_c50, testSet["model"], type='prob')$pred
+testSet$OOF_pred_rf <- predict(model_rf, testSet[, predictors])
+testSet$OOF_pred_knn <- predict(model_knn, testSet[, predictors])
+testSet$OOF_pred_c50 <- predict(model_c50, testSet[, predictors])
 
 predictors_top<-c('OOF_pred_rf','OOF_pred_knn','OOF_pred_c50')
 
-model_glm <- train(trainSet[, predictors_top], 
-                   trainSet[, "model"],
+model_glm <- train(testSet[, predictors_top], 
+                   testSet[, outcome],
                    method='glm',
                    trControl = fitControl,
                    tuneLength = 3)
 
 
-model_gbm <- train(trainSet[, predictors_top], 
-                   trainSet[, "model"],
+model_gbm <- train(testSet[, predictors_top], 
+                   testSet[, outcome],
                    method='gbm',
                    trControl = fitControl,
                    tuneLength = 3)
