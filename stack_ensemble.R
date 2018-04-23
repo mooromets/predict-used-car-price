fae9315e4c4e54074d16762465fae15ff963d5ba
mooromets <sergey.sambor@gmail.com>
2018-04-23 10:27:20 +0200

    stacking ensemble first draft

--- stack_ensemble.R
+++ stack_ensemble.R
@@ -1,61 +1,64 @@
 library(caret)
-library(caretEnsemble)
 
 source("R/ML_utils.R")
 
 set.seed(1357)
-dataset <- VWSample(4000)
+trainSet <- VWSample(4000)
 
-#setup
-seed <- 798
-metric <- "Accuracy"
-control <- trainControl(method="cv", number=4)
+set.seed(7531)
+testSet <- VWSample(1000)
+
+outcome <- c("model") # Y column
+predictors <- colnames(trainSet)[!colnames(trainSet) %in% outcome] # X columns
+
 preProc <- c("center", "scale")
 
+# train 3 models of the low level
+control <- trainControl(method="cv", 
+                        number=6, 
+                        savePredictions = 'final', 
+                        classProbs = T)
 
-## learning 
-results <- resamples(
-  c(sapply(X = c("rf", "C5.0", "treebag", "PART", "J48"),
-           FUN = defModelFit,
-           seed = seed,
-           form = model ~ .,
-           data = dataset,
-           metric = metric,
-           trControl = control,
-           simplify = FALSE,
-           USE.NAMES = TRUE),
-    sapply(X = c("svmRadial", "knn"),
-           FUN = defModelFit,
-           seed = seed,
-           form = model ~ .,
-           data = dataset,
-           metric = metric,
-           trControl = control,
-           preProcess = c("center", "scale"),
-           simplify = FALSE,
-           USE.NAMES = TRUE)))
+model_rf <- train(x = trainSet[, predictors], 
+                  y = trainSet[, outcome], 
+                  method = "rf", 
+                  trControl = control)
 
-#summary(results)
-#dotplot(results)
+model_knn <- train(trainSet[, predictors], 
+                   trainSet[, outcome], 
+                   method = "knn", 
+                   trControl = control, 
+                   preProcess = preProc)
 
-# correlation between results
-modelCor(results)
-splom(results)
+model_c50 <- train(trainSet[, predictors], 
+                   trainSet[, outcome], 
+                   method = "C5.0", 
+                   trControl = control)
+
+#add low-level predictions to BOTH datasets
+trainSet$OOF_pred_rf <- model_rf$pred$pred[order(model_rf$pred$rowIndex)]
+trainSet$OOF_pred_knn <- model_knn$pred$pred[order(model_knn$pred$rowIndex)]
+trainSet$OOF_pred_c50 <- model_c50$pred$pred[order(model_c50$pred$rowIndex)]
+
+testSet$OOF_pred_rf <- predict(model_rf, testSet[, predictors])
+testSet$OOF_pred_knn <- predict(model_knn, testSet[, predictors])
+testSet$OOF_pred_c50 <- predict(model_c50, testSet[, predictors])
+
+predictors_top<-c('OOF_pred_rf', 'OOF_pred_knn', 'OOF_pred_c50')
+
+# train top-level method
+model_gbm <- train(trainSet[, predictors_top], 
+                   trainSet[, outcome],
+                   method='gbm',
+                   trControl = control,
+                   tuneLength = 3)
 
 
-#stacking
-algorithmList <- c('C5.0', 'svmRadial')
-models <- caretList(model ~ ., data=dataset, 
-                    trControl=control, methodList=algorithmList)
+#predict using GBM top layer model
+testSet$gbm_stacked <- predict(model_gbm, testSet[, predictors_top])
 
-# stack using glm
-stackControl <- trainControl(method="cv", number=4, savePredictions=TRUE, classProbs=TRUE)
-set.seed(seed)
-stack.glm <- caretStack(models, method="glm", metric="Accuracy", trControl=stackControl)
-#ERROR: Not yet implemented for multiclass problems
-#print(stack.glm)
+#check results
+cMat <- confusionMatrix(testSet$gbm_stacked, testSet$model)
 
-set.seed(seed)
-stack.rf <- caretStack(models, method="rf", metric="Accuracy", trControl=stackControl)
-#ERROR: Not yet implemented for multiclass problems
-#print(stack.rf)
+print(cMat$byClass)
+print(cMat$overall)
